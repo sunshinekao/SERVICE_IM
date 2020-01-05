f9af6c9866eba6b384e39b684c51d3edbc5f37be
sunshinekao <sunshinekao@users.noreply.github.com>
2020-01-05 12:40:10 +0800

    登陆注册接口设计
    测试接口ok

--- src/main/java/net/qiujuer/web/italker/push/Application.java
+++ src/main/java/net/qiujuer/web/italker/push/Application.java
@@ -1,6 +1,8 @@
 package net.qiujuer.web.italker.push;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import net.qiujuer.web.italker.push.provider.AuthRequestFilter;
+import net.qiujuer.web.italker.push.provider.GsonProvider;
 import net.qiujuer.web.italker.push.service.AccountService;
 import org.glassfish.jersey.server.ResourceConfig;
 
@@ -9,14 +11,19 @@ import java.util.logging.Logger;
 /**
  * @author qiujuer
  */
-public class Application extends ResourceConfig{
-    public Application(){
+public class Application extends ResourceConfig {
+    public Application() {
         // 注册逻辑处理的包名
-//        packages("net.qiujuer.web.italker.push.service");
+        //packages("net.qiujuer.web.italker.push.service");
         packages(AccountService.class.getPackage().getName());
 
+        // 注册我们的全局请求拦截器
+        register(AuthRequestFilter.class);
+
         // 注册Json解析器
-        register(JacksonJsonProvider.class);
+//         register(JacksonJsonProvider.class);
+        // 替换解析器为Gson
+        register(GsonProvider.class);
 
         // 注册日志打印输出
         register(Logger.class);
